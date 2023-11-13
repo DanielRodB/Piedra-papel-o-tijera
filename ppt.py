ff836ec71a8f1e6ce29a015634c9f167c9a678f5
Daniel Al <alexro_2002@hotmail.com>
2023-11-13 15:32:49 -0600

    Actualización del juego con dos nuevas opciones

--- ppt.py
+++ ppt.py
@@ -1,22 +1,31 @@
-print("Escribe una de las letras correspondientes para jugar piedra, papel o tijera contra la máquina")
+print("Escribe una de las letras correspondientes para jugar piedra, papel, tijera, lagarto o spock contra la máquina")
 print("a) piedra")
 print("b) papel")
 print("c) tijera")
+print("d) lagarto")
+print( "e) spock")
 import random
-opciones = ("piedra" , "papel" , "tijera")
+opciones = ("piedra" , "papel" , "tijera" , "lagarto" , "spock")
 c = input ()
-if c == ("a") : print ("Eligió piedra, la máquina jugará con: ") 
-if c == ("b") : print ("Eligió papel, la máquina jugará con: ")
-if c == ("c") : print ("Eligió tijera, la máquina jugará con: ")
+if c == ("a") : print("Eligió piedra, la máquina jugará con: ") 
+if c == ("b") : print("Eligió papel, la máquina jugará con: ")
+if c == ("c") : print("Eligió tijera, la máquina jugará con: ")
+if c == ("d") : print("Eligió lagarto, la máquina jugará con:") 
+if c == ("e") : print("Eligió spock, la máquina jugará con:") 
 seleccion = random.choice(opciones) 
 print(seleccion)
-if seleccion== "piedra": 
+if seleccion == "piedra": 
     if c == "a":
         print("Empate")
     elif c == "b": 
         print("La máquina ganó") 
-    elif c == "c" :
+    elif c == "c":
         print("Usted ganó")   
+    elif c == "d":
+        print ("La piedra aplastó al lagarto, ganó")
+    elif c == "e" :
+        print("la piedra fue destruida por spock, perdió")
+        
 elif seleccion == "papel":
     if c == "a" :
         print("La máquina ganó")
@@ -24,11 +33,43 @@ elif seleccion == "papel":
        print("Empate ")
     elif c == "c":
         print("Usted ganó")
+    elif c == "d":
+        print ("El lagartó se comió el papel, perdió")
+    elif c == "e" :
+        print("El papel desaprueba a Spock, ganó")
+
 elif seleccion == "tijera":
     if c == "a":
        print("usted ganó")
     elif c == "b":
         print("ganó la máquina")
     elif c == "c":
-        print ("nadie ganó")
+        print ("Empate")
+    elif c == "d":
+        print ("Las tijeras decapitaron al lagarto, ganó")
+    elif c == "e":
+        print("Spock destruyó las tijeras, perdió")
 
+elif seleccion == "lagarto":
+    if c == "a":
+       print("El lagarto fue aplastado por la piedra, ganó")
+    elif c == "b":
+        print("El lagaro se comió el papel, perdió")
+    elif c == "c":
+        print ("Las tijeras decapitaron al lagarto, ganó")
+    elif c == "d":
+        print ("Empate")
+    elif c == "e" :
+        print("El lagarto envenenó a spock, perdió")
+
+elif seleccion == "spock":
+    if c == "a":
+       print("La piedra fue destruida por Spock, perdió ")
+    elif c == "b":
+        print("El papel desaprueba a Spock, ganó")
+    elif c == "c":
+        print ("Spock destruyó las tijeras, perdió")
+    elif c == "d":
+        print ("Spock fue envenenado, ganó")
+    elif c == "e" :
+        print("Empate")
